a78bb1791ccc3a79999ec82ac0f4158ff07db397
sharpattack <sharp1209@gmail.com>
2014-08-13 14:48:20 -0500

    included the missing require:image in controller, textbook params

--- app/controllers/textbooks_controller.rb
+++ app/controllers/textbooks_controller.rb
@@ -52,6 +52,6 @@ class TextbooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def textbook_params
-      params.require(:textbook).permit(:Title, :Description)
+      params.require(:textbook).permit(:Title, :Description, :image)
     end
 end
