3752d8eeb558f5a683468d8a4cfe1e87901ead3c
sharpattack <sharp1209@gmail.com>
2014-08-13 15:16:46 -0500

    Added validator to the textbook model

--- app/models/textbook.rb
+++ app/models/textbook.rb
@@ -2,4 +2,6 @@ class Textbook < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100"}
+
+	validates_attachment_content_type :image, :content_type => /\Aimage/
 end
